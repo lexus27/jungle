5d94fe47d53601feba65be81b8c0a55c7aac3def
Alexey <Lexus.1995@mail.ru>
2016-09-30 12:17:45 +1000

    Большая чистка

--- public/lab/communication/url.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alexey
- * Date: 08.03.2016
- * Time: 14:27
- */
--- public/lab/model/mapping.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by Kutuzov Alexey Konstantinovich <lexus.1995@mail.ru>.
- * Author: Kutuzov Alexey Konstantinovich <lexus.1995@mail.ru>
- * Project: jungle
- * IDE: PhpStorm
- * Date: 22.05.2016
- * Time: 19:24
- */
