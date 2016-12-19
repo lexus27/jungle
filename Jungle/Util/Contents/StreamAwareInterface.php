3d9e49ab09338581101b56324e45255a9371e9fb
Alexey <Lexus.1995@mail.ru>
2016-12-20 09:25:07 +1000

    add: StreamAwareInterface

---
+++ Jungle/Util/Contents/StreamAwareInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Kutuzov Alexey Konstantinovich <lexus.1995@mail.ru>.
+ * Author: Kutuzov Alexey Konstantinovich <lexus.1995@mail.ru>
+ * Project: jungle
+ * IDE: PhpStorm
+ * Date: 14.12.2016
+ * Time: 22:43
+ */
+namespace Jungle\Util\Contents {
+
+	/**
+	 * Interface StreamAwareInterface
+	 * @package Jungle\Util\Contents
+	 */
+	interface StreamAwareInterface{
+
+		public function getStream();
+
+	}
+}
+
