bb399d7cc0269cb31f13e7c4cbce41678d070839
lexus27 <lexus27.khv@gmail.com>
2018-03-15 17:57:16 +1000

    fix: Request not defined headers

--- Jungle/Http/Request.php
+++ Jungle/Http/Request.php
@@ -105,13 +105,25 @@ namespace Jungle\Http {
 				return \getallheaders();
 			}
 			
-			$headers = [];
-			foreach($_SERVER as $key=>$value){
-				if(preg_match('@HTTP_(.+)@',$key,$result)){
-					$headers[$result[1]]=$value;
+			// fix Accept header a not found on nginx
+			if(!is_array($_SERVER)){
+				return array();
+			}
+			$headers = array();
+			foreach ($_SERVER as $name => $value){
+				if (substr($name, 0, 5) == 'HTTP_') {
+					$headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
 				}
 			}
 			return $headers;
+			
+			//$headers = [];
+			//foreach($_SERVER as $key=>$value){
+			//	if(preg_match('@HTTP_(.+)@',$key,$result)){
+			//		$headers[$result[1]]=$value;
+			//	}
+			//}
+			//return $headers;
 		}
 		
 		/**
